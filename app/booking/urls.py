443cbadb7e3624d0d35cf45f0575fbbc05455d1a
Wander Bravo <wander.bravo@gmail.com>
2020-03-27 12:09:37 -0500

    Fix template issues and redirects

--- app/booking/urls.py
+++ app/booking/urls.py
@@ -23,6 +23,7 @@ from upload.views import image_upload
 from calendars.views import signup
 
 urlpatterns = [
+    path("", image_upload, name="upload"),
     path("home/", image_upload, name="upload"),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
--- app/upload/views.py
+++ app/upload/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render,redirect
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm 
 
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect("/")
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect("/")
-        else:
-            return render(request, "signup.html", {"form": form})
-    else:
-        form = UserCreationForm()
-        return render(request, 'signup.html', {'form': form})
-
+@login_required(login_url='/login/')
 def image_upload(request):
     if request.method == "POST" and request.FILES["image_file"]:
         image_file = request.FILES["image_file"]
